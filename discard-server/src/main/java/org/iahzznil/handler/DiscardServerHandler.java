5f850602107da5353389e98e1aede6d6b7d7d4b9
ryan <ryan2022022@gmail.com>
2024-01-03 18:09:54 +0400

    first commit

---
+++ discard-server/src/main/java/org/iahzznil/handler/DiscardServerHandler.java
@@ -0,0 +1,12 @@
+package org.iahzznil.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ctx.fireChannelRead(msg);
+    }
+}
