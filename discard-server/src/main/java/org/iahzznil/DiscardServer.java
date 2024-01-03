b473cdbfa49b07af002bd7e94c94391cf0727fa2
ryan <ryan2022022@gmail.com>
2024-01-03 18:28:08 +0400

    echo & receive

--- discard-server/src/main/java/org/iahzznil/DiscardServer.java
+++ discard-server/src/main/java/org/iahzznil/DiscardServer.java
@@ -10,6 +10,7 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.iahzznil.handler.DiscardServerHandler;
+import org.iahzznil.handler.EchoServerHandler;
 
 /**
  * 丢弃任何进入的数据
@@ -32,7 +33,7 @@ public class DiscardServer {
                     .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
                         @Override
                         public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new DiscardServerHandler());
+                            ch.pipeline().addLast(new EchoServerHandler());
                         }
                     })
                     .option(ChannelOption.SO_BACKLOG, 128)          // (5)
--- discard-server/src/main/java/org/iahzznil/handler/ReceiveServerHandler.java
+++ discard-server/src/main/java/org/iahzznil/handler/ReceiveServerHandler.java
@@ -5,7 +5,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.ReferenceCountUtil;
 
-public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+public class ReceiveServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
