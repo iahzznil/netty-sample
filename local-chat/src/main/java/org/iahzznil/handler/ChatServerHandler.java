92a5f967e16145b29ea1dcc768a0799fe7fc3667
ryan <ryan2022022@gmail.com>
2024-01-04 10:46:03 +0400

    chat server

---
+++ local-chat/src/main/java/org/iahzznil/handler/ChatServerHandler.java
@@ -0,0 +1,13 @@
+package org.iahzznil.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class ChatServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ctx.write(msg);
+        ctx.flush();
+    }
+}
